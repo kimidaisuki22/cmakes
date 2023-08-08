a6ef3024763f4962b48a511e219d5491721090bd
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-08-08 08:46:24 -0400

    add run in test script

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-set -x 
+PS4='\n\033[1;32m#[$0:$LINENO] \033[0m' ;set -x 
 cmake --preset default && cmake --build --preset default 
+
+cmake --build --preset default --target run_foo
 
 
 cmake --workflow --preset default
+
+
